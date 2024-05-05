47b33faae1cfbf04b3d6684a324168785755680f
tuando169 <dodangtuan609@gmail.com>
2024-05-05 09:00:42 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LoginPage from '@/components/LoginPage.vue'
+</script>
+
+<template>
+  <LoginPage />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100..900;1,100..900&family=Noto+Sans&display=swap');
+
+#app {
+  font-family: 'MuseoModerno', sans-serif;
+}
+</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="trangchu">
+        <div class="gmail">
+          <img
+            alt="logo"
+            style="width: 91px; height: 85px"
+          />
+          <h1>HI, HỒNG LÊ</h1>
+          <p class="text_gmail">
+            VietDoodleGallery muốn đảm bảo rằng chính bạn đang <br />
+            cố gắng truy cập vào admin.vietdoodlegallery.com
+          </p>
+          <button class="add_email">
+           
+            <select id="email" name="email">
+              <option value="email1@example.com">hongle1229@gmail.com</option>
+              <option value="email2@example.com">xuanphong1@gmail.com</option>
+              <option value="email3@example.com">dtm123@gmail.com</option>
+              <!-- Add more options as needed -->
+          </select>
+          </button>
+        </div>
+
+        <div class="login">
+          <p>
+            Để tiếp tục, trước tiên, hãy đăng nhập để xác minh danh tính của bạn
+          </p>
+          <div class="form_field">
+            <input type="password" class="form_input" placeholder=""/> 
+            <label for="name" class="form_label">Nhập mật khẩu</label>
+            <i class="fa-solid fa-eye-slash" id="hienmatkhau" onclick="hienmatkhau()"></i>
+            <i class="fa-solid fa-eye" id="hienmatkhau" onclick="hienmatkhau()"></i>
+          </div>
+          <div class="check">
+            <input type="checkbox" checked="checked" name="checked" />
+            <p style="padding-left: 10px">Lưu đăng nhập</p>
+          </div>
+
+          <div class="forget">
+            <a href="#" id="forget">Quên mật khẩu?</a>
+            <button class="next"><a href="#" id="forget1">Tiếp theo</a></button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
